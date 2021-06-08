5c51bf412a6d4c636a47afae89c06055c43f3f3a
SowmyaGunda <sowmya.gunda9@gmail.com>
2021-06-08 15:47:14 +0100

    removed dead code

--- app/src/main/java/com/example/chatapplication/view/MainActivity.kt
+++ app/src/main/java/com/example/chatapplication/view/MainActivity.kt
@@ -1,13 +1,8 @@
 package com.example.chatapplication.view
 
-import android.animation.Animator
-import android.animation.AnimatorListenerAdapter
-import android.animation.AnimatorSet
-import android.animation.ObjectAnimator
 import android.os.Bundle
 import android.os.Handler
 import android.view.View
-import android.view.animation.OvershootInterpolator
 import android.widget.Button
 import android.widget.EditText
 import android.widget.ImageButton
@@ -92,7 +87,6 @@ class MainActivity : AppCompatActivity() {
     }
 
     private fun onSendClicked(msg: String, view: View) {
-       // handleSendButtonClick(message, view)
         if (msg.isNotEmpty()) {
             viewModel.sendMessage(repository, MessageItem(msg, MessageItem.TYPE_MY_MESSAGE, System.currentTimeMillis(), true))
             message.text.clear()
@@ -106,35 +100,4 @@ class MainActivity : AppCompatActivity() {
         val n: Int = rand.nextInt(randomReplies.size - 1)
         return MessageItem(randomReplies[n], MessageItem.TYPE_FRIEND_MESSAGE, System.currentTimeMillis(), true)
     }
-
-    private fun handleSendButtonClick(composeMessage: EditText, view: View) {
-        this.handler.postDelayed({
-            val viewHolder: RecyclerView.ViewHolder? = chatList.findViewHolderForAdapterPosition(chatAdapter.itemCount - 1)
-            if (viewHolder is ChatAdapter.MyMessageViewHolder) {
-                val outgoingReplyViewHolder: ChatAdapter.MyMessageViewHolder = viewHolder
-                val outgoingMessageView: View = outgoingReplyViewHolder.messageContent
-                val composeMessagePos = IntArray(2)
-                composeMessage.getLocationInWindow(composeMessagePos)
-                val startX = composeMessagePos[0]
-                val startY = composeMessagePos[1]
-                val newMessagePos = IntArray(2)
-                outgoingMessageView.getLocationInWindow(newMessagePos)
-                val x = newMessagePos[0] - startX
-                val y = newMessagePos[1] - startY
-                val flySet = AnimatorSet()
-                val flyX: Animator = ObjectAnimator.ofFloat(outgoingMessageView, View.TRANSLATION_X, x.toFloat())
-                val flyY: Animator = ObjectAnimator.ofFloat(outgoingMessageView, View.TRANSLATION_Y, y.toFloat())
-                flySet.playTogether(flyX, flyY)
-                flySet.duration = 250
-                flySet.interpolator = OvershootInterpolator(1.0f)
-                flySet.addListener(object : AnimatorListenerAdapter() {
-                    override fun onAnimationEnd(animation: Animator) {
-                        outgoingReplyViewHolder.itemView.visibility = View.VISIBLE
-                    }
-                })
-                flySet.start()
-            }
-        }, messageAnimator.addDuration)
-    }
-
 }
